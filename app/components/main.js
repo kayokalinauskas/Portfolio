a36e06c704694b23a63abcfb6e967d1e0d7e24a5
kayokalinauskas <kalinauskas@live.com>
2023-10-11 01:35:22 -0300

    ajust responsiveness

--- app/components/main.js
+++ app/components/main.js
@@ -6,19 +6,16 @@ export default async function Main({ locale, namespaces }) {
   const { t } = await initTranslations(locale, namespaces);
   return (
     <section className={styles["about-section"]}>
-      <div>
+      <div className={styles["presentation-div"]}>
         <p className={styles.greeting}>{t("greeting")}</p>
         <h1>Kayo Kalinauskas</h1>
         <h2 className={styles.subtitle}>&lt;{t("frontenddev")}/&gt;</h2>
         <p>{t("presentation")}</p>
         <button>{t("resumedownload")}</button>
       </div>
-      <Image
-        src="./assets/ProgrammerImage.svg"
-        width={550}
-        height={550}
-        alt="Teste"
-      />
+      <div className={styles["image-container"]}>
+        <Image src="./assets/ProgrammerImage.svg" fill={true} alt="Teste" />
+      </div>
     </section>
   );
 }
