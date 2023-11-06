4de9bd59275d08a292dbce38b39a5ba7b9235366
kayokalinauskas <kalinauskas@live.com>
2023-11-06 19:44:54 -0300

    Ajusted Footer and Resume

--- app/_components/footer/Footer.js
+++ app/_components/footer/Footer.js
@@ -8,50 +8,13 @@ export default function Footer() {
   const { t } = useTranslation();
   return (
     <footer className={styles["footer-section"]}>
-      {/* <div className={styles["icons-div"]}>
-        <Link
-          target="_blank"
-          href="https://www.linkedin.com/in/kayokalinauskas/"
-        >
-          <Image
-            className={styles["icon-image"]}
-            width={32}
-            height={32}
-            src="/assets/linkedin.svg"
-            alt="Kayo Kalinauskas Linkedin"
-          />
-        </Link>
-        <Link target="_blank" href="https://github.com/kayokalinauskas/">
-          <Image
-            className={styles["icon-image"]}
-            width={32}
-            height={32}
-            src="/assets/github.svg"
-            alt="Kayo Kalinauskas Github"
-          />
-        </Link>
-        <Link target="_blank" href="mailto:kalinauskas@live.com">
-          <Image
-            className={styles["icon-image"]}
-            width={32}
-            height={32}
-            src="/assets/email.svg"
-            alt="Kayo Kalinauskas Email"
-          />
-        </Link>
-      </div>
-      <p className={styles["footer-logo"]}>&lt;/kayokalinauskas&gt;</p> */}
-      <p>
-        {t("footer")}
-        <br></br>
-        <Link
-          target="_blank"
-          href="https://www.linkedin.com/in/kayokalinauskas/"
-        >
-          {" "}
-          Kayo Kalinauskas
-        </Link>{" "}
-      </p>
+      <Link
+        target="_blank"
+        href="https://github.com/kayokalinauskas/Portfolio/"
+      >
+        <p>{t("footer")}</p>
+        <span>Kayo Kalinauskas</span>
+      </Link>
     </footer>
   );
 }
